812660ff1bc00e10d7cf99a962559a203467aa32
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-01-22 05:17:14 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -62,7 +62,7 @@ def runner
   first_hand = initial_round
   prompt_user
   get_user_input
-  display_card_total(card_total)
+  display_card_total(first_hand)
   card_total = hit?(first_hand)
   until card_total >= 21
     display_card_total(card_total)
