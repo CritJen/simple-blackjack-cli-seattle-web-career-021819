1eeb7beb5339786125251cbf8f12902e96b8ec59
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-01-22 05:34:33 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,34 +1,54 @@
+require 'pry'
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
+  rand(1...11)
   # code #deal_card here
 end
 
-def display_card_total
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
   # code #display_card_total here
 end
 
 def prompt_user
+  puts "Type 'h' to hit or 's' to stay"
   # code #prompt_user here
 end
 
 def get_user_input
+  gets.chomp
   # code #get_user_input here
 end
 
-def end_game
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
   # code #end_game here
 end
 
 def initial_round
-  # code #initial_round here
+  card_1 = deal_card
+  card_2 = deal_card
+  display_card_total(card_1 + card_2)
+  return card_1 + card_2
 end
 
-def hit?
-  # code hit? here
+def hit?(card_total)
+  prompt_user
+  user_input = get_user_input
+  if user_input == 'h'
+    card_total += deal_card
+  else if user_input == 's'
+  else
+    invalid_command
+    prompt_user
+  end
 end
+return card_total
+end
+
 
 def invalid_command
   # code invalid_command here
@@ -37,8 +57,12 @@ end
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
-
 def runner
-  # code runner here
+  welcome
+  first_hand = initial_round
+  until first_hand >= 21
+    first_hand = hit?(first_hand)
+    display_card_total(first_hand)
+  end
+  end_game(first_hand)
 end
-    
